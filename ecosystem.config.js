b29fab19aca9d6371297b685cf9cd6fd66177ddf
Hannah Rogers <hannah@redskytech.io>
2021-10-11 14:16:58 -0600

    updated config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,36 +2,21 @@ module.exports = {
     apps: [{
         name: "app",
         script: "app.js"
-    }],
+    }]
     deploy: {
-        // "production" is the environment name
-        production: {
-            // SSH key path, default to $HOME/.ssh
-            key: "~/.ssh",
-            // SSH user
+        sand: {
+            key: '~/.ssh/id_rsa.pub',
             user: "dev",
-            // SSH host
-            host: ["192.168.0.13"],
-            // SSH options with no command-line flag, see 'man ssh'
-            // can be either a single string or an array of strings
-            ssh_options: "StrictHostKeyChecking=no",
-            // GIT remote/branch
-            ref: "origin/master",
-            // GIT remote
-            repo: "git@github.com:hannah10e/hello-world2.git",
-            // path in the server
-            // path: "/var/www/my-repository",
-            // Pre-setup command or path to a script on your local machine
-            // 'pre-setup': "apt-get install git ; ls -la",
-            // Post-setup commands or path to a script on the host machine
-            // eg: placing configurations in the shared dir etc
-            'post-setup': "ls -la",
-            // pre-deploy action
-            'pre-deploy-local': "echo 'This is a local executed command'",
-            // post-deploy action
-            'post-deploy': "npm install",
-        },
+            host: ["45.79.84.132"],
+            ref: 'origin/master',
+            repo: 'git@github.com:hannah10e/hello-world.git',
+            path: "/home/dev/sand/hello-world",
+            'post-deploy': "pm2 startOrReload ecosystem.config.js",
+        }
     }
 }
 
-// source: https://pm2.io/docs/runtime/guide/easy-deploy-with-ssh/
+// sources:
+// https://pm2.keymetrics.io/docs/usage/application-declaration/
+// https://pm2.io/docs/runtime/guide/easy-deploy-with-ssh/
+// https://gist.github.com/hoangmirs/b2cb60e0aa60019f0c8b13927ce9d0a2
