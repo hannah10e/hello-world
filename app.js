4caadfa073d697a750321b24526e3eff47803f1c
Hannah Rogers <hannah@redskytech.io>
2021-10-15 09:53:45 -0600

    added deploy action tests

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+const express = require('express');
+const app = express();
+const port = 3005;
+app.get('/', (req, res) => {
+    res.send('HELLO WORLD');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
